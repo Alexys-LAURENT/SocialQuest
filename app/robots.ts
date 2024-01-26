5425e4bf2bfc93c79a19a60fb71aba5ae995e854
Louis <louis.lamouche2204@gmail.com>
2024-01-26 17:37:02 +0100

    robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: '/messages',
+        },
+        sitemap: `${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml`,
+    }
+}
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,7 @@
 import { getAllUsernames } from "@/utils/sitemap/getAllUsernames";
 import { getAllPosts } from "@/utils/sitemap/getAllPosts";
 import { getAllGuildes } from "@/utils/sitemap/getAllGuildes";
+import { MetadataRoute } from "next";
 
 const staticRoutes = [
     "/",
@@ -12,7 +13,7 @@ const staticRoutes = [
 ]
 
 
-export default async function sitemap() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap[]> {
 
     const [allUsernames, allPosts, allGuildes] = await Promise.all([
         getAllUsernames(),
@@ -24,7 +25,7 @@ export default async function sitemap() {
         return {
             url: `${process.env.NEXT_PUBLIC_APP_URL}${item}`,
             lastModified: new Date(),
-        }
+        } as unknown as MetadataRoute.Sitemap
     })
 
 
@@ -32,28 +33,28 @@ export default async function sitemap() {
         return {
             url: `${process.env.NEXT_PUBLIC_APP_URL}/${item.username}`,
             lastModified: new Date(),
-        }
+        } as unknown as MetadataRoute.Sitemap
     })
 
     const inventaires = allUsernames.map((item) => {
         return {
             url: `${process.env.NEXT_PUBLIC_APP_URL}/${item.username}/inventaire`,
             lastModified: new Date(),
-        }
+        } as unknown as MetadataRoute.Sitemap
     })
 
     const posts = allPosts.map((item) => {
         return {
             url: `${process.env.NEXT_PUBLIC_APP_URL}/p/${item.id_post}`,
             lastModified: new Date(),
-        }
+        } as unknown as MetadataRoute.Sitemap
     })
 
     const guildes = allGuildes.map((item) => {
         return {
             url: `${process.env.NEXT_PUBLIC_APP_URL}/g/${item.nom}`,
             lastModified: new Date(),
-        }
+        } as unknown as MetadataRoute.Sitemap
     })
 
     return [
