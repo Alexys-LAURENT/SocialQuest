e948149760e8d28b416bda62ed8ed3148f3e829f
Alexys LAURENT <103429337+Alexys-LAURENT@users.noreply.github.com>
2024-02-16 17:43:54 +0100

    prevent unauthenticated user go page missions

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,25 @@
-import { NextResponse, NextRequest } from 'next/server'
-import { createClient } from '@/utils/supabase/middleware'
+import { NextResponse, NextRequest } from 'next/server';
+import { createClient } from '@/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
   try {
-    const { supabase, response } = createClient(request)
+    const { supabase, response } = createClient(request);
 
     // Get the current session
-    const session = await supabase.auth.getSession()
+    const session = await supabase.auth.getSession();
 
     // If a session exists and the user is trying to access the login page, redirect them to the home page
     if (session.data.session && request.nextUrl.pathname.startsWith('/login')) {
-      return NextResponse.redirect(new URL('/', request.url))
+      return NextResponse.redirect(new URL('/', request.url));
     }
 
     // If a session does not exist and the user is trying to access a protected page, redirect them to the login page
     if (!session.data.session && request.nextUrl.pathname.startsWith('/messages')) {
-      return NextResponse.redirect(new URL('/login', request.url))
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    if (!session.data.session && request.nextUrl.pathname.startsWith('/missions')) {
+      return NextResponse.redirect(new URL('/login', request.url));
     }
 
     if (request.nextUrl.pathname.startsWith('/g')) {
@@ -23,25 +27,23 @@ export async function middleware(request: NextRequest) {
         .from('guildes')
         .select('id_guilde')
         .eq('nom', request.nextUrl.pathname.split('/')[2])
-        .single()
+        .single();
       if (error || !data) {
-        console.error(error)
-        return NextResponse.redirect(new URL('/', request.url))
+        console.error(error);
+        return NextResponse.redirect(new URL('/', request.url));
       }
     }
 
-    return response
+    return response;
   } catch (e) {
     return NextResponse.next({
       request: {
         headers: request.headers,
       },
-    })
+    });
   }
 }
 
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.svg).*)',
-  ],
-}
+  matcher: ['/((?!_next/static|_next/image|favicon.svg).*)'],
+};
