86dfe82d818b3391d75bd57b5f25683743b973c6
Alexys LAURENT <103429337+Alexys-LAURENT@users.noreply.github.com>
2024-02-19 17:38:54 +0100

    sticky to user guildes section on home page

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default async function Index() {
         <PostsWrapper user={user} getPost={getAllPosts} filtre={user && user.id_user ? true : false} />
       </div>
 
-      <div className="hidden lg:flex min-w-[17rem] h-fit">
+      <div className="sticky top-0  hidden lg:flex min-w-[17rem] h-fit">
         {user ?
           (
            <UserGuildes/>
