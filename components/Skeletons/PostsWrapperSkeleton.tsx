65d0088311770a96a38b01fabf40d750fa19c12e
Alexys LAURENT <103429337+Alexys-LAURENT@users.noreply.github.com>
2024-02-22 15:33:51 +0100

    reduce the number of post skeleton to 5

--- components/Skeletons/PostsWrapperSkeleton.tsx
+++ components/Skeletons/PostsWrapperSkeleton.tsx
@@ -1,18 +1,11 @@
 import PostSkeleton from '@/components/Skeletons/PostSkeleton';
 
 const PostsWrapperSkeleton = ({ postPage }: { postPage?: boolean }) => {
-
-
-
-    return (
-        <div className={`w-full flex flex-col gap-4 mb-4 ${postPage ? 'mt-8' : ''}`}>
-            {
-                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]?.map((item: number) => (
-                    <PostSkeleton key={`postWrapperSkeleton-${item}`} />
-                ))
-            }
-        </div>
-    );
+  return (
+    <div className={`w-full flex flex-col gap-4 mb-4 ${postPage ? 'mt-8' : ''}`}>
+      {[1, 2, 3, 4, 5]?.map((item: number) => <PostSkeleton key={`postWrapperSkeleton-${item}`} />)}
+    </div>
+  );
 };
 
 export default PostsWrapperSkeleton;
