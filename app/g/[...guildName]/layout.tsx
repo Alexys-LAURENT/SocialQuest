25dc9cb2193ff67daf82e5ae9bc5c011ec83302c
Alexys LAURENT <103429337+Alexys-LAURENT@users.noreply.github.com>
2024-02-23 10:29:58 +0100

    removed console.log | Works fine

--- app/g/[...guildName]/layout.tsx
+++ app/g/[...guildName]/layout.tsx
@@ -40,8 +40,6 @@ export async function generateStaticParams() {
 
   const returnData = params?.concat(paramsWithActivities!, paramsWithGuildWars!);
 
-  console.log(returnData);
-
   return returnData;
 }
 
